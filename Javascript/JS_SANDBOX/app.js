add0e5a6a1f75dd484fde698892eb2787de9cfba
Tusharj09 <36297059+Tusharj09@users.noreply.github.com>
2020-12-26 00:48:41 +0530

    feat: learningmaterials
    learning material  till mathObject

---
+++ Javascript/JS_SANDBOX/app.js
@@ -0,0 +1,198 @@
+// alert("Hello World");
+
+// //log to console
+// console.log('hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = 'Hello World';
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1,b:2});
+// console.table({a:1,b:2});
+
+
+// console.error("This is an error");
+// //console.clear();
+// console.warn("This is an warning");
+// console.time("Hello");
+// console.log('hello World');
+// console.log('hello World');
+// console.log('hello World');
+// console.log('hello World');
+// console.timeEnd("Hello");
+
+
+
+//-------------------------------------section 2------------------------------------------
+                                    //VARIABLES
+
+// var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
+// LET
+// let name;
+// name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+
+// CONST
+// const name = 'John';
+// console.log(name);
+// Can not reassign
+// name = 'Sara';
+// Have to assign a value
+// const greeting;
+
+// const person = {
+//     name: 'John',
+//     age: 30
+//   }
+  
+//   person.name = 'Sara';
+//   person.age = 32;
+  
+//   // console.log(person);
+  
+//   const numbers = [1,2,3,4,5];
+//   numbers.push(6);
+  
+//   console.log(numbers);
+
+
+//-------------------------------------------section 3 -------------------------------------------
+                                    //DATATYPE
+
+                                // // PRIMITIVE
+
+// // String
+// const name = 'John Doe';
+// // Number
+// const age = 45;
+// // Boolean
+// const hasKids = true;
+// // Null
+// const car = null;
+// // Undefined
+// let test;
+// // Symbol
+// const sym = Symbol();
+
+
+                    // REFERENCE TYPES - Objects
+// // Array
+// const hobbies = ['movies', 'music'];
+// // Object literal
+// const address = {
+//   city: 'Boston',
+//   state: 'MA'
+// }
+// const today = new Date();s
+// console.log(today);
+// console.log(typeof today);
+
+
+
+//-----------------------------------------------section-4-------------------------------
+                                       
+                                          // TYPE CONVERSION
+
+// let val;
+
+// // Number to string
+// val = String(555);
+// val = String(4+4);
+// // Bool to string
+// val = String(true);
+// // Date to string
+// val = String(new Date());
+// // Array to string
+// val = String([1,2,3,4]);
+
+// // toString()
+// val = (5).toString();
+// val = (true).toString();
+
+// // String to number
+// val = Number('5');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number('hello');
+// val = Number([1,2,3]);
+
+// val = parseInt('100.30');
+// val = parseFloat('100.30');
+
+// // Output
+// // console.log(val);
+// // console.log(typeof val);
+// // //console.log(val.length);
+// // console.log(val.toFixed(2));
+
+// const val1 = String(5);
+// const val2 = 6;
+// const sum = Number(val1 + val2);
+
+// console.log(sum);
+// console.log(typeof sum);
+
+         
+//--------------------------------------------------section-5-------------------------------------
+
+                                        // Number and math Object
+
+// const num1 = 100;
+// const num2 = 50;
+// let val;
+
+// // Simple math with numbers
+// val = num1 + num2;
+// val = num1 * num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+// // Math Object
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.4);
+// val = Math.ceil(2.4);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+// val = Math.min(2,33,4,1,55,6,3,-2);
+// val = Math.max(2,33,4,1,55,6,3,-2);
+// val = Math.random();
+
+// val = Math.floor(Math.random() * 20 + 1);
+
+// console.log(val);
+
+
+//--------------------------------------------------section-6-------------------------------
+                                        //String
+
+                                        
+                                        
