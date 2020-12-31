04e82983df1cc7632d57e46db708fe230a6de302
Tusharj09 <36297059+Tusharj09@users.noreply.github.com>
2021-01-01 02:39:11 +0530

    feat : Create,RemoveandReplace ELments
    Coded material based on above technology

--- Javascript/DOM/app.js
+++ Javascript/DOM/app.js
@@ -134,57 +134,148 @@
 
 //---------------------------------------------------Traversing through DOM--------------------------------
 
+// let val;
+
+// const list = document.querySelector('ul.collection');
+// const listItem = document.querySelector('li.collection-item:first-child');
+
+// val = listItem;
+// val = list;
+
+// // Get child nodes
+// val = list.childNodes;
+// val = list.childNodes[0];
+// val = list.childNodes[0].nodeName;
+// val = list.childNodes[3].nodeType;
+
+// // 1 - Element
+// // 2 - Attribute (deprecated)
+// // 3 - Text node
+// // 8 - Comment
+// // 9 - Document itself
+// // 10 - Doctype
+
+
+// // Get children element nodes
+// val = list.children;
+// val = list.children[1];
+// list.children[1].textContent = 'Hello';
+// // Children of children
+// list.children[3].children[0].id = 'test-link';
+// val = list.children[3].children[0];
+
+// // First child
+// val = list.firstChild;
+// val = list.firstElementChild;
+
+// // Last child
+// val = list.lastChild;
+// val = list.lastElementChild;
+
+// // Count child elements
+// val = list.childElementCount;
+
+// // Get parent node
+// val = listItem.parentNode;
+// val = listItem.parentElement;
+// val = listItem.parentElement.parentElement;
+
+// // Get next sibling
+// val = listItem.nextSibling;
+// val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+// // Get prev sibling
+// val = listItem.previousSibling;
+// val = listItem.previousElementSibling;
+// console.log(val);
+
+//----------------------------------------------------Creating Elements-------------------------------------
+
+// // Create element
+// const li  = document.createElement('li');
+
+// // Add class
+// li.className = 'collection-item';
+
+// // Add id
+// li.id = 'new-item';
+
+// // Add attribute
+// li.setAttribute('title', 'New Item');
+
+// // Create text node and append
+// li.appendChild(document.createTextNode('Hello World'));
+
+// // Create new link element
+// const link = document.createElement('a');
+// // Add classes
+// link.className = 'delete-item secondary-content';
+// // Add icon html
+// link.innerHTML = '<i class="fa fa-remove"></i>';
+
+// // Append link into li
+// li.appendChild(link);
+
+// // Append li as child to ul
+// document.querySelector('ul.collection').appendChild(li);
+
+// console.log(li);
+
+
+//---------------------------------------Replace and Remove the elements---------------------------------------
+// Replace the Elements
+
+//Create an Elements
+const newHeading = document.createElement('h2');
+// Add Id
+newHeading.id = 'task-tile';
+//new test node
+newHeading.appendChild(document.createTextNode('Task List'));
+
+//Get the Old heading 
+const oldHeading = document.getElementById('task-title');
+//Parent
+const cardAction = document.querySelector('.card-action');
+
+// Replace
+cardAction.replaceChild(newHeading,oldHeading);
+
+//REMOVE ELEMENTS
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+//Remove List Item 
+lis[0].remove();
+
+//Remove the  Child ELements
+
+list.removeChild(lis[3]);
+
+// CLASSES AND ATTRIBUTE
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
+
 let val;
 
-const list = document.querySelector('ul.collection');
-const listItem = document.querySelector('li.collection-item:first-child');
+// Classes
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+link.classList.remove('test');
+val = link;
 
-val = listItem;
-val = list;
+// Attributes
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+link.setAttribute('title', 'Google');
+val = link.hasAttribute('title');
+link.removeAttribute('title');
+val = link;
 
-// Get child nodes
-val = list.childNodes;
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName;
-val = list.childNodes[3].nodeType;
-
-// 1 - Element
-// 2 - Attribute (deprecated)
-// 3 - Text node
-// 8 - Comment
-// 9 - Document itself
-// 10 - Doctype
-
-
-// Get children element nodes
-val = list.children;
-val = list.children[1];
-list.children[1].textContent = 'Hello';
-// Children of children
-list.children[3].children[0].id = 'test-link';
-val = list.children[3].children[0];
-
-// First child
-val = list.firstChild;
-val = list.firstElementChild;
-
-// Last child
-val = list.lastChild;
-val = list.lastElementChild;
-
-// Count child elements
-val = list.childElementCount;
-
-// Get parent node
-val = listItem.parentNode;
-val = listItem.parentElement;
-val = listItem.parentElement.parentElement;
-
-// Get next sibling
-val = listItem.nextSibling;
-val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
-
-// Get prev sibling
-val = listItem.previousSibling;
-val = listItem.previousElementSibling;
 console.log(val);
+
+
+
+
+
