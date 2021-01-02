a5ae5b656f54689d7ec6b0cd7c525b81cc684d21
Tusharj09 <36297059+Tusharj09@users.noreply.github.com>
2021-01-03 00:15:18 +0530

    feat : Event bubbling adn delegation

--- Javascript/DOM/app.js
+++ Javascript/DOM/app.js
@@ -353,7 +353,7 @@
 
 
 
-//-----------------------------------------keyBoard inputs adn Events---------------------------------------
+//-----------------------------------------keyBoard inputs and Events---------------------------------------
 
 
 // const form = document.querySelector('form');
@@ -399,9 +399,41 @@
 // }
 
 
-//---------------------------------------EVENT BUBBLING----------------------------------------------
+//---------------------------------------EVENT BUBBLING and DELEGATION----------------------------------------------
 
-document.querySelector('.card-title').addEventListener('click',function(){
+// EVENT BUBBLING
 
-    console.log('card title');
-});
+// document.querySelector('.card-title').addEventListener('click', function(){
+//   console.log('card title');
+// });
+
+// document.querySelector('.card-content').addEventListener('click', function(){
+//   console.log('card content');
+// });
+
+// document.querySelector('.card').addEventListener('click', function(){
+//   console.log('card');
+// });
+
+// document.querySelector('.col').addEventListener('click', function(){
+//   console.log('col');
+// });
+
+// EVENT DELGATION
+
+// const delItem = document.querySelector('.delete-item');
+
+// delItem.addEventListener('click', deleteItem);
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e){
+  // if(e.target.parentElement.className === 'delete-item secondary-content'){
+  //   console.log('delete item');
+  // }
+
+  if(e.target.parentElement.classList.contains('delete-item')){
+    console.log('delete item');
+    e.target.parentElement.parentElement.remove();
+  }
+}
