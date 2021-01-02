0e2d6e7af26d3676367c40fc8f66e0c6f36fe7c1
Tusharj09 <36297059+Tusharj09@users.noreply.github.com>
2021-01-02 23:37:14 +0530

    feat : keyboard inpu and events

--- Javascript/DOM/app.js
+++ Javascript/DOM/app.js
@@ -283,73 +283,125 @@
 //   //e.preventDefault();
 // });
 
-document.querySelector('.clear-tasks').addEventListener('click', onClick);
+// document.querySelector('.clear-tasks').addEventListener('click', onClick);
 
-function onClick(e){
-  //console.log('Clicked');
+// function onClick(e){
+//   //console.log('Clicked');
 
-  let val;
+//   let val;
 
-  val = e;
+//   val = e;
 
-  // Event target element
-  val = e.target;
-  val = e.target.id;
-  val = e.target.className;
-  val = e.target.classList;
+//   // Event target element
+//   val = e.target;
+//   val = e.target.id;
+//   val = e.target.className;
+//   val = e.target.classList;
 
-  // Event type
-  val = e.type;
+//   // Event type
+//   val = e.type;
 
-  // Timestamp
-  val = e.timeStamp;
+//   // Timestamp
+//   val = e.timeStamp;
 
-  // Coords event relative to the window
-  val = e.clientY;
-  val = e.clientX;
+//   // Coords event relative to the window
+//   val = e.clientY;
+//   val = e.clientX;
 
-  // Coords event relative to the element
-  val = e.offsetY;
-  val = e.offsetX;
+//   // Coords event relative to the element
+//   val = e.offsetY;
+//   val = e.offsetX;
 
-  console.log(val);
-}
+//   console.log(val);
+// }
 
 
 //-----------------------------------------------Mouse Event---------------------------
 
 
-const clearBtn = document.querySelector('.clear-tasks');
-const card = document.querySelector('.card');
-const heading = document.querySelector('h5');
+// const clearBtn = document.querySelector('.clear-tasks');
+// const card = document.querySelector('.card');
+// const heading = document.querySelector('h5');
 
-// Click
-// clearBtn.addEventListener('click', runEvent);
-// Doubleclick
-// clearBtn.addEventListener('dblclick', runEvent);
-// Mousedown
-// clearBtn.addEventListener('mousedown', runEvent);
-// Mouseup
-// clearBtn.addEventListener('mouseup', runEvent);
-// Mouseenter
-// card.addEventListener('mouseenter', runEvent);
-// Mouseleave
-// card.addEventListener('mouseleave', runEvent);
-// Mouseover
-// card.addEventListener('mouseover', runEvent);
-// Mouseout
-// card.addEventListener('mouseout', runEvent);
-// Mousemove
-card.addEventListener('mousemove', runEvent);
+// // Click
+// // clearBtn.addEventListener('click', runEvent);
+// // Doubleclick
+// // clearBtn.addEventListener('dblclick', runEvent);
+// // Mousedown
+// // clearBtn.addEventListener('mousedown', runEvent);
+// // Mouseup
+// // clearBtn.addEventListener('mouseup', runEvent);
+// // Mouseenter
+// // card.addEventListener('mouseenter', runEvent);
+// // Mouseleave
+// // card.addEventListener('mouseleave', runEvent);
+// // Mouseover
+// // card.addEventListener('mouseover', runEvent);
+// // Mouseout
+// // card.addEventListener('mouseout', runEvent);
+// // Mousemove
+// card.addEventListener('mousemove', runEvent);
 
-// Event Handler
-function runEvent(e) {
-  console.log(`EVENT TYPE: ${e.type}`);
+// // Event Handler
+// function runEvent(e) {
+//   console.log(`EVENT TYPE: ${e.type}`);
 
-  heading.textContent= `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+//   heading.textContent= `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
 
-  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
-}
+//   document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+// }
 
 
 
+//-----------------------------------------keyBoard inputs adn Events---------------------------------------
+
+
+// const form = document.querySelector('form');
+// const taskInput = document.getElementById('task');
+// const heading = document.querySelector('h5');
+// const select = document.querySelector('select');
+
+// // Clear input
+// taskInput.value = '';
+
+// // form.addEventListener('submit', runEvent);
+
+// // Keydown
+// //taskInput.addEventListener('keydown', runEvent);
+// // Keydown
+// // taskInput.addEventListener('keyup', runEvent);
+// // Keypress
+// // taskInput.addEventListener('keypress', runEvent);
+// // Focus
+// // taskInput.addEventListener('focus', runEvent);
+// // Blur
+// // taskInput.addEventListener('blur', runEvent);
+// // Cut
+// // taskInput.addEventListener('cut', runEvent);
+// // Paste
+// // taskInput.addEventListener('paste', runEvent);
+// // Input
+// // taskInput.addEventListener('input', runEvent);
+// // Change
+// select.addEventListener('change', runEvent);
+
+// function runEvent(e){
+//   console.log(`EVENT TYPE: ${e.type}`);
+
+//   //console.log(e.target.value);
+
+//   // heading.innerText = e.target.value;
+
+//   // Get input value
+//   // console.log(taskInput.value);
+
+//   // e.preventDefault();
+// }
+
+
+//---------------------------------------EVENT BUBBLING----------------------------------------------
+
+document.querySelector('.card-title').addEventListener('click',function(){
+
+    console.log('card title');
+});
