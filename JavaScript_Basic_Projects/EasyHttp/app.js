f947670517102a7e132f32c2f6eb9ff0f2e50b05
Tusharj09 <36297059+Tusharj09@users.noreply.github.com>
2021-01-30 23:24:04 +0530

    Ajax CRUD
    materails

---
+++ JavaScript_Basic_Projects/EasyHttp/app.js
@@ -0,0 +1,67 @@
+const http = new easyHTTP;
+
+//Get posts 
+
+// const post = http.get('https://jsonplaceholder.typicode.com/posts',function(err,response){
+   
+//  if(err){
+//     console.log(err);
+//  }
+// else {
+//     console.log(response);
+// }
+// });
+
+
+//Single POst
+// http.get('https://jsonplaceholder.typicode.com/posts/1',function(err,post){
+   
+//  if(err){
+//     console.log(err);
+//  }
+// else {
+//     console.log(post);
+// }
+// });
+
+
+
+//Create Data
+const data = {
+    title : 'Custom Post',
+    body: 'This is a custom post'
+};
+// POst
+
+http.post('https://jsonplaceholder.typicode.com/posts',data,function(err,post){
+    if(err){
+            console.log(err);
+         }
+        else {
+            console.log(post);
+        }
+});
+
+
+//Update Post
+http.put('https://jsonplaceholder.typicode.com/posts/1',data,function(err,post){
+    if(err){
+            console.log(err);
+         }
+        else {
+            console.log(post);
+        }
+});
+
+
+//Delete Post
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1',function(err,response){
+   
+ if(err){
+    console.log(err);
+ }
+else {
+    console.log(response);
+}
+});
