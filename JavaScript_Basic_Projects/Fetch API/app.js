91c42e26e5a53d1cf4c1878f6d24870f08b3903a
Tusharj09 <36297059+Tusharj09@users.noreply.github.com>
2021-01-31 21:12:07 +0530

    New material Promises
    promises

---
+++ JavaScript_Basic_Projects/Fetch API/app.js
@@ -0,0 +1,13 @@
+document.getElementById('button1').addEventListener('click',getText);
+
+
+function getText(){
+
+    fetch('test.txt')
+    .then(function(res){
+       return res.text();
+    }).then(function(data) {
+        console.log(data);
+    })
+
+}
