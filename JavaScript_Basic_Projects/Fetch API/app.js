73eca767805e0d302d572f46ac189d2160ebf7c9
Tusharj09 <36297059+Tusharj09@users.noreply.github.com>
2021-02-01 03:25:38 +0530

    feat : fetchAPI
    GEt by fetch API

--- JavaScript_Basic_Projects/Fetch API/app.js
+++ JavaScript_Basic_Projects/Fetch API/app.js
@@ -1,5 +1,10 @@
 document.getElementById('button1').addEventListener('click',getText);
 
+document.getElementById('button2').addEventListener('click',getJson);
+document.getElementById('button2').addEventListener('click',getExternal);
+
+
+
 
 function getText(){
 
@@ -8,6 +13,42 @@ function getText(){
        return res.text();
     }).then(function(data) {
         console.log(data);
-    })
+    }).catch(function(err){
+        console.log(err);
+    });
+
+}
+
+
+function getJson(){
+    
+    fetch('posts.json')
+    .then(function(res){
+       return res.json();
+    }).then(function(data) {
+        console.log(data);
+    }).catch(function(err){
+        console.log(err);
+    });
+
+}
+
+
+
+function getExternal(){
+    fetch('https://api.github.com/users')
+    .then(function(res){
+       return res.json();
+    }).then(function(data) {
+        console.log(data);
+        let output = '';
+        data.forEach(function(user){
+            output += `<li>${user.login}</li>`
+        })
+ 
+        document.getElementById('output').innerHTML = output;
+    }).catch(function(err){
+        console.log(err);
+    });
 
 }
